7ba67b1e9529141651cf334d6dcd88d4a67c9a14
Amir Yakhin <amir.yakhin94@google.com>
2025-11-07 00:58:08 -0500

    Trigger Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/REPO_NAME/",    // <-- replace with your repo name
+  base: "/UNI/",    // <-- replace with your repo name
   plugins: [react()],
   build: { outDir: "dist" }
 });
